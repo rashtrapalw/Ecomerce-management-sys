ad9d84f2a7e22bc5d661e20fb1669074bbc077d3
rashtrapal143 <rashtraapalw143@gmail.com>
2025-09-18 16:47:26 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AppNavbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import ProductList from "./pages/ProductList";
 import AddProduct from "./pages/AddProduct";
 import EditProduct from "./pages/EditProduct";
 
 function App() {
   return (
-    <Router>
-      <AppNavbar />
-      <Routes>
-        <Route path="/" element={<ProductList />} />
-        <Route path="/add" element={<AddProduct />} />
-        <Route path="/edit/:id" element={<EditProduct />} />
-      </Routes>
-    </Router>
+    <div>
+      <Navbar />
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/add" element={<AddProduct />} />
+          <Route path="/edit/:id" element={<EditProduct />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
